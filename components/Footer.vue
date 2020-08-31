0434dfce4e8404b4c118014a726afbd4d2b00082
Kimon Topouzidis <kimon.topouzidis@gmail.com>
2020-08-31 12:29:40 +0300

    added Header and Footer

---
+++ components/Footer.vue
@@ -0,0 +1,19 @@
+<template>
+  <footer class="footer bg-gray-300 w-full">
+    <div class="flex flex-col xs:flex-row items-center justify-center xs:justify-between  px-3 py-2">
+      <div class="footer__info">
+        MovieFinder © {{ new Date().getFullYear() }}
+      </div>
+      <div class="footer__made">
+        Made by Kimon Topouzidis
+      </div>
+    </div>
+  </footer>
+</template>
+
+<script>
+export default {}
+</script>
+
+<style>
+</style>
